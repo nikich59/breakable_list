15124c9de31be8c569399ce267e15220fc9a7506
nikich59 <nik-prokoshev@yandex.ru>
2019-04-18 01:03:49 +0300

    initial commit

---
+++ app/src/main/java/com/prokoshevnik/breakablelist/BreakableListExampleActivity.kt
@@ -0,0 +1,16 @@
+package com.prokoshevnik.breakablelist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.prokoshevnik.R
+import com.prokoshevnik.uiutils.breakablelist.BreakableListLayout
+
+class BreakableListExampleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_breakable_list_example)
+
+        val breakableList = findViewById<BreakableListLayout>(R.id.breakable_list_example_view)
+    }
+}
---
+++ breakablelist/src/main/java/com/prokoshevnik/uiutils/breakablelist/BreakableList.kt
@@ -0,0 +1,110 @@
+package com.prokoshevnik.uiutils.breakablelist
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.ViewGroup
+import android.widget.FrameLayout
+
+
+class BreakableListLayout(context: Context, attrs: AttributeSet?, defStyle: Int) :
+        FrameLayout(context, attrs, defStyle) {
+    constructor(context: Context) : this(context, null, 0)
+
+    constructor(context: Context, attrs: AttributeSet) : this(context, attrs, 0)
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        var currentTopEndX = 0
+        var currentTopEndY = 0
+
+        var rowHeight = 0
+
+        val width = MeasureSpec.getSize(widthMeasureSpec)
+
+        for (childIndex in 0.until(childCount)) {
+            val child = getChildAt(childIndex)
+
+            child.measure(0, 0)
+
+            val childMarginLeft = (child.layoutParams as ViewGroup.MarginLayoutParams).leftMargin
+            val childMarginTop = (child.layoutParams as ViewGroup.MarginLayoutParams).topMargin
+            val childMarginRight = (child.layoutParams as ViewGroup.MarginLayoutParams).rightMargin
+            val childMarginBottom = (child.layoutParams as ViewGroup.MarginLayoutParams).bottomMargin
+
+            val childWidth = child.measuredWidth
+            val childHeight = child.measuredHeight
+
+            if (currentTopEndX + childMarginLeft + childWidth + childMarginRight > width) {
+                currentTopEndY += rowHeight
+
+                currentTopEndX = childMarginLeft + childWidth + childMarginRight
+                rowHeight = 0
+            } else {
+                currentTopEndX += childMarginLeft + childWidth + childMarginRight
+            }
+
+            if (childMarginTop + childHeight + childMarginBottom > rowHeight)
+                rowHeight = childMarginTop + childHeight + childMarginBottom
+        }
+
+        setMeasuredDimension(width, currentTopEndY + rowHeight)
+    }
+
+    override fun onLayout(p0: Boolean, p1: Int, p2: Int, p3: Int, p4: Int) {
+        var currentTopEndX = 0
+        var currentTopEndY = 0
+
+        var rowHeight = 0
+
+        for (childIndex in 0.until(childCount)) {
+            val child = getChildAt(childIndex)
+
+            child.measure(0, 0)
+
+            val childMarginLeft = (child.layoutParams as ViewGroup.MarginLayoutParams).leftMargin
+            val childMarginTop = (child.layoutParams as ViewGroup.MarginLayoutParams).topMargin
+            val childMarginRight = (child.layoutParams as ViewGroup.MarginLayoutParams).rightMargin
+            val childMarginBottom = (child.layoutParams as ViewGroup.MarginLayoutParams).bottomMargin
+
+            val childWidth = child.measuredWidth
+            val childHeight = child.measuredHeight
+
+            if (currentTopEndX + childMarginLeft + childWidth + childMarginRight > width) {
+                currentTopEndY += rowHeight
+
+                child.layout(childMarginLeft,
+                        currentTopEndY + childMarginTop,
+                        childMarginLeft + childWidth,
+                        currentTopEndY + childMarginTop + childHeight)
+
+                currentTopEndX = childMarginLeft + childWidth + childMarginRight
+                rowHeight = 0
+            } else {
+                child.layout(currentTopEndX + childMarginLeft,
+                        currentTopEndY + childMarginTop,
+                        currentTopEndX + childMarginLeft + childWidth,
+                        currentTopEndY + childMarginTop + childHeight)
+
+                currentTopEndX += childMarginLeft + childWidth + childMarginRight
+            }
+
+            if (childMarginTop + childHeight + childMarginBottom > rowHeight)
+                rowHeight = childMarginTop + childHeight + childMarginBottom
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
